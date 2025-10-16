6a7c08692276bcba52ba15dfa28fb31cb163cbc5
Eatzaz <alhafizatzaz111@gmail.com>
2025-10-16 10:20:34 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Eatzaz Hafiz on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
