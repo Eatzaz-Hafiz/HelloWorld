fd6d1baa378b0d132b07141498fdb47424b94406
Eatzaz <alhafizatzaz111@gmail.com>
2025-10-19 00:11:11 +0300

    testing

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,16 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var goal = "Week"
+    @State private var selectedDuration: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            ZStack{
+                Button("􀙭"){
+                    
+                }
+                .frame(width: 109, height: 109)
+                .background(Color.color1)
+                .clipShape(Circle())
+                .glassEffect()
+                
+//                Image("fire")
+//                    .frame(width: 109, height: 109)
+//                    .background(Color.horange)
+//                    .clipShape(Circle())
+//
+//                    .padding()
+                
+
+                
+            }
+            .padding()
+            
+            VStack(alignment: .leading , spacing: 16 ) {
+                
+                Text("Hello Learner")
+                    .font(.system(size: 34, weight: .bold, design: .default))
+                
+                
+                Text("This app will help you learn everyday!")
+                    .font(.body)
+                    .foregroundColor(.secondary)
+                
+                Text("I want to learn ")
+                
+                TextField("Swift", text: .constant(""))
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 393, height: 48)
+                    .padding()
+                Text("I want to learn it in a")
+                
+                HStack(spacing: 16) {
+                    ForEach(["Week", "Month", "Year"], id: \.self) { duration in
+                        Button(duration) {
+                            selectedDuration = duration
+                        }
+                        .frame(width: 97, height: 48)
+                        .background(selectedDuration == duration ? Color.hOrange : Color.hGray.opacity(0.1))
+                        .foregroundColor(.white)
+                        .cornerRadius(30)
+                        .glassEffect()
+                    }
+                }
+            }
+                Spacer()
+                
+                    .padding()
+                
+                Button ("Start learning"){}
+                    .frame(width: 182, height: 48)
+                    .background(Color.hOrange)
+                    .foregroundColor(.white)
+                    .cornerRadius(30)
+                    .glassEffect()
+                
+                
+            }
         }
-        .padding()
     }
-}
+
+            
 
 #Preview {
     ContentView()
