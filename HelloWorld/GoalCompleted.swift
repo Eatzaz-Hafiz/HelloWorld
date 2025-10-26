db8cf6dec39ffd421cdace3c9c1a04206991a095
Eatzaz <alhafizatzaz111@gmail.com>
2025-10-26 19:32:10 +0300

    calendar Done ✅

---
+++ HelloWorld/GoalCompleted.swift
@@ -0,0 +1,18 @@
+//
+//  GoalComplete.swift
+//  HelloWorld
+//
+//  Created by Eatzaz Hafiz on 26/10/2025.
+//
+
+import SwiftUI
+
+struct GoalComplete: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalComplete()
+}
